c95b17a4de33622d7ed62051e02b4d15494a70f9
smunozr <sergiomunru6@hotmail.com>
2020-02-18 19:59:13 +0100

    18-02-20

---
+++ PRUEBASCLASE/Test.php
@@ -0,0 +1,11 @@
+<?php
+include "operaciones.php";
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testSum(){
+        $this->assertTrue();
+    }
+}
---
+++ PRUEBASCLASE/operaciones.php
@@ -0,0 +1,17 @@
+<?php
+
+    function suma($a,$b){
+       return ($a+$b);
+    }
+
+    function resta($a,$b){
+        return ($a-$b);
+    }
+
+    function mult($a,$b){
+        return ($a*$b);
+    }
+
+    function div($a,$b){
+        return ($a/$b);
+    }
